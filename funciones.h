906aed0bf09d8a3573148e8dabf7a4f5945eda1f
carbone2001 <47418180+carbone2001@users.noreply.github.com>
2019-03-29 17:38:58 -0300

    modificacion documentacion funciones

--- funciones.h
+++ funciones.h
@@ -1,31 +1,31 @@
-/** \brief Realiza una operacion de suma
- * \param Dos variables de tipo flotante
+/** \brief Realiza una operacion de suma y devuelve el resultado
+ * \param x e y son las variables de tipo flotante que se utilizaran para operar.
  * \return El resultado de tipo flotante de la suma de las variables ingresadas.
  */
 float fsumar(float x, float y);
 
 
-/** \brief Realiza una operacion de resta
- * \param Dos variables de tipo flotante
+/** \brief Realiza una operacion de resta y devuelve el resultado
+ * \param x e y son las variables de tipo flotante que se utilizaran para operar.
  * \return El resultado de tipo flotante de la resta de las variables ingresadas.
  */
 float frestar(float x, float y);
 
-/** \brief Realiza una operacion de division
- * \param Dos variables de tipo flotante
+/** \brief Realiza una operacion de division y devuelve el resultado
+ * \param x e y son las variables de tipo flotante que se utilizaran para operar.
  * \return El resultado de tipo flotante de la division de las variables ingresadas.
  */
 float fdivision(float x, float y);
 
-/** \brief Realiza una operacion de multiplicacion
- * \param Dos variables de tipo flotante
+/** \brief Realiza una operacion de multiplicacion y devuelve el resultado
+ * \param x e y son las variables de tipo flotante que se utilizaran para operar.
  * \return El resultado de tipo flotante de la multiplicacion de las variables ingresadas.
  */
 float fmultiplicacion(float x, float y);
 
-/** \brief Realiza una operacion de factorial
- * \param Dos variables de tipo flotante
- * \return El resultado de tipo flotante del factorial de las variables ingresadas.
+/** \brief Realiza una operacion de factorial y devuelve el resultado
+ * \param x e y son las variables de tipo entero que se utilizaran para operar.
+ * \return El resultado de tipo entero del factorial de las variables ingresadas.
  */
 int ffactorial(int x);
 
